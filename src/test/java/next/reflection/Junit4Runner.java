bec68b1d65e3e45965e33920d36a5d602e4ccf87
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-12 17:29:07 +0900

    요구사항3  완료

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -2,11 +2,21 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4Runner {
     @Test
     public void run() throws Exception {
         Class clazz = Junit4Test.class;
 
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+
+        }
     }
 }
 

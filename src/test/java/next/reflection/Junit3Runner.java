29900bcf9490d0ea0bafe41813228cff93f63fa1
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-12 16:44:19 +0900

    요구사항2 완료

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -2,10 +2,18 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit3Runner {
     @Test
     public void runner() throws Exception {
         Class clazz = Junit3Test.class;
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for(Method method : methods) {
+            method.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
 
     }
 }
